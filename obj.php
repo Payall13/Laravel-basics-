098eac9efd06a1ced0c05ec7bbfdf5e033681a8e
payall918 <110537306+payall918@users.noreply.github.com>
2022-12-24 16:40:10 +0530

    Create obj.php

---
+++ obj.php
@@ -0,0 +1,45 @@
+<?php
+class test
+{
+	function abc ()
+	{
+		echo"hello students";
+		echo "<br>";
+	}
+}
+	$obj=new test;
+	$obj->abc();
+?>
+
+<?php
+class addition
+{
+	function add ()
+	{
+		$a = 12;
+		$b = 5;
+		$c = $a + $b;
+	
+		
+		 echo"$c";
+		 echo"<br>";
+	}
+}
+	$obj= new addition;
+	$obj->add();
+		
+?>
+
+<?php
+class college
+{
+	function name()
+	{
+		echo "geetanjali";
+		echo "<br>";
+		echo "saurashtra";
+	}
+}
+	$obj= new college;
+	$obj->name();
+?>
